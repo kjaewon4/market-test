b49c520cd9aa5f05c64bf107ec3280370efbeb19
nadanote <jaewon0002@gmail.com>
2025-02-04 20:13:27 +0900

    메인, Nav

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export default function Nav({ mode, setMode }) {
+  const [activeLink, setActiveLink] = useState(''); // 활성화된 링크 상태 관리
+
+  const handleClick = (event, linkMode) => {
+    event.preventDefault();
+    setMode(linkMode);
+    setActiveLink(linkMode); // 클릭한 링크의 상태 업데이트
+  };
+
+  return (
+    <div className='Nav'>
+      <a href="/create"
+        className={activeLink === 'CREATE' ? 'active' : ''}
+        onClick={(event) => handleClick(event, 'CREATE')}>상품 등록
+      </a>
+      <span> | </span>
+      <a href="/like"
+        className={activeLink === 'LIKE' ? 'active' : ''}
+        onClick={(event) => handleClick(event, 'LIKE')}>찜 목록
+      </a>
+    </div>
+  )
+}
+
